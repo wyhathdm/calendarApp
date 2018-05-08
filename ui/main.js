d71f2ac3fddd5130f745288a8075e8a7a195def6
wyhathdm <falserthanfake1+wyhathdm@gmail.com>
2018-05-07 23:13:38 -0400

    fixed buttons

--- ui/main.js
+++ ui/main.js
@@ -6,15 +6,27 @@ function loadMon(){
 let month = createMonth(curMon, curYear);
 
 for(i = 0; i<42; i++){
-    $('#monthName').html(month.name);
+    $('#monthName').html(month.name + "  " + month.year);
     $("#day"+i).html(month.month[i]);
 }
 
 }
 
-$("#fwdBtn").click(function(){
-    alert("hi");
+function forward(){
     curMon++;
+    if(curMon>11){
+        curYear++;
+        curMon = 0;
+    }
     loadMon();
-});
+}
+
+function backward(){
+    curMon--;
+    if(curMon<0){
+        curYear--;
+        curMon = 11;
+    }
+    loadMon();
+}
 
