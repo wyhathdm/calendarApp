8ad7e8c0bf06ed9e9333f142b2a116ffc8c775a8
Jon <jon@example.com>
2018-05-07 20:28:48 -0400

    added month and other data

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
-var running = true;
-var calDaysInMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-var weekDays = ["Su","M ","Tu","W ","Th","F ","Sa"];
-var monthStarts = [4,0,0,3,5,1,3,6,2,4,0,2];
+let running = true;
+let calDaysInMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+let monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+let weekDays = ["Su","M ","Tu","W ","Th","F ","Sa"];
+let monthStarts = [4,0,0,3,5,1,3,6,2,4,0,2];
 
-var events = []
+let events = []
 
-month = [];
+let month = [];
+
+for(i=0; i<42; i++){
+    month.push(null);
+}
+
+console.table(month);
+
+function repString(string, amnt){
+    let output = "";
+    for(i = 0; i<amnt; i++){
+        output+=string;
+    }
+    return output;
+}
+
