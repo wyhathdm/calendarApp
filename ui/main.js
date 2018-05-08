483e6b992c1997efde2e1d2b093ac2075fb79ff1
wyhathdm <falserthanfake1+wyhathdm@gmail.com>
2018-05-07 23:08:45 -0400

    fixed things, added buttons

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,20 @@
-function loadScr(){
+let curMon = 1;
+let curYear = 2018;
 
-let month = createMonth(0, 2018);
+function loadMon(){
+
+let month = createMonth(curMon, curYear);
+
 for(i = 0; i<42; i++){
-    $("#day"+i).html(i);
+    $('#monthName').html(month.name);
+    $("#day"+i).html(month.month[i]);
 }
+
 }
+
+$("#fwdBtn").click(function(){
+    alert("hi");
+    curMon++;
+    loadMon();
+});
 
