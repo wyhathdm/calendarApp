6921375e52a51089da198134a66f4a3b05bf17fc
Jon <jon@example.com>
2018-05-07 20:19:23 -0400

    add html

---
+++ main.js
@@ -0,0 +1,9 @@
+var running = true;
+var calDaysInMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var weekDays = ["Su","M ","Tu","W ","Th","F ","Sa"];
+var monthStarts = [4,0,0,3,5,1,3,6,2,4,0,2];
+
+var events = []
+
+month = [];
